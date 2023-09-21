afa6bd49cef1caf679f592a43211bfdc8e09741c
Mathis-Richard <mathis.richard.mtp@outlook.com>
2023-09-21 10:44:20 +0200

    Mise à jour fichiers docker

---
+++ html/script.js
@@ -0,0 +1,11 @@
+let xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+xhr.open('GET', 'http://localhost:8081/api/test/first');
+xhr.setRequestHeader('accept', '*/*');
+
+xhr.onload = function() {
+    document.getElementById('test').innerHTML = xhr.response;
+    console.log(xhr.response);
+};
+
+xhr.send();
