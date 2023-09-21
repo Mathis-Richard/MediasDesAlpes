9ddafc3634f62652d8f7993bb005c7943065e250
Mathis-Richard <mathis.richard.mtp@outlook.com>
2023-09-21 10:44:20 +0200

    Mise à jour fichiers docker

--- src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/controller/TestController.java
+++ src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/controller/TestController.java
@@ -1,2 +1,17 @@
-package fr.univtln.cnam.bdd.groupe5.mediasdesalpes.controller;public class TestController {
+package fr.univtln.cnam.bdd.groupe5.mediasdesalpes.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "null", originPatterns = {"http://localhost:[*]"}, allowedHeaders = "*", allowCredentials = "true")
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/first")
+    public String premierTest(){
+        return "It works";
+    }
 }
