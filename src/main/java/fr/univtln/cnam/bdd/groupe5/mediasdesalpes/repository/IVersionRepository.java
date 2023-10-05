583ab44610efc4b348c636fef11706e728a3bbe0
Mathis-Richard <mathis.richard.mtp@outlook.com>
2023-10-05 23:54:47 +0200

    Architecture JPA fin prête

--- src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/repository/IVersionRepository.java
+++ src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/repository/IVersionRepository.java
@@ -1,16 +1,13 @@
 package fr.univtln.cnam.bdd.groupe5.mediasdesalpes.repository;
 
-import fr.univtln.cnam.bdd.groupe5.mediasdesalpes.model.Test;
+import fr.univtln.cnam.bdd.groupe5.mediasdesalpes.model.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ITestRepository extends JpaRepository<Test,Integer> {
+public interface IVersionRepository extends JpaRepository<Version,Long> {
 
     @Query(value = "SELECT version()", nativeQuery = true)
     String getVersion();
-
-    @Query(value = "SELECT * FROM coucou", nativeQuery = true)
-    Integer getTest();
 }
--- src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/repository/impl/TestRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package fr.univtln.cnam.bdd.groupe5.mediasdesalpes.repository.impl;
-
-import fr.univtln.cnam.bdd.groupe5.mediasdesalpes.repository.ITestRepository;
-import org.springframework.stereotype.Repository;
-
-public class TestRepositoryImpl {
-
-}
