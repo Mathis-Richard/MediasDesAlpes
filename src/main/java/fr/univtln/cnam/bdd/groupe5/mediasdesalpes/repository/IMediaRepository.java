3e7c4ab996cc545b080d7220d2b1ffae7fd557c4
Mathis-Richard <mathis.richard.mtp@outlook.com>
2023-11-17 14:09:49 +0100

    Conteneurisation finalisée + début de front

--- src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/repository/IMediaRepository.java
+++ src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/repository/IMediaRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IMediaRepository extends JpaRepository<MediaEntity,Long> {
 
     @Query(value = "SELECT * FROM Media WHERE idmedia = :id", nativeQuery = true)
     MediaEntity getMediaById(Integer id);
 
+    @Query(value = "SELECT * FROM Media", nativeQuery = true)
+    List<MediaEntity> getMedia();
 }
--- src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/service/IMediaService.java
+++ src/main/java/fr/univtln/cnam/bdd/groupe5/mediasdesalpes/service/IMediaService.java
@@ -3,7 +3,11 @@ package fr.univtln.cnam.bdd.groupe5.mediasdesalpes.service;
 import fr.univtln.cnam.bdd.groupe5.mediasdesalpes.model.Media;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface IMediaService {
+
+    List<Media> getMedia();
     Media getMediaById(Integer id);
 }
